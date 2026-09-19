a8a946743e6137e52b36d768e50496637dcb4d47
Jay Dev <jaydev787748@gmail.com>
2026-09-20 01:17:43 +0530

    chore: export new control-plane security APIs

--- core/security/__init__.py
+++ core/security/__init__.py
@@ -1,4 +1,4 @@
-from .environment import (
+from .capabilities import CapabilityBoundary, CapabilityBoundaryError\nfrom .environment import EnvironmentPolicyError, build_sandbox_environment, is_sensitive_name\nfrom .evidence import SecurityEvidence, build_security_evidence\nfrom .hardening import HardeningController, HardeningEvidence\nfrom .network import NetworkBoundary, NetworkBoundaryError\nfrom .posture import (\n    FilesystemMode, FilesystemPosture, NetworkEgress, NetworkMode, ProcessQuota,\n    SecretBoundary, SecretMode, SecurityAuditReport, SecurityAuditor, SecurityProfile,\n    SecurityPostureError, SyscallMode,\n)\nfrom .quotas import QuotaError, QuotaLedger, ResourceQuota\nfrom .race import FenceError, FenceToken, OwnershipFence\nfrom .secrets import SecretBoundaryScanner, SecretScan\nfrom .translation import ContainerSecurityEnvelope, compile_security_envelope, security_arguments\nfrom .environment import (
     EnvironmentPolicyError,
     build_sandbox_environment,
     is_sensitive_name,
