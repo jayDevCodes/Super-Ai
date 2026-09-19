cd15614948948b1d2e4433e022d1740d3d11540d
Jay Dev <jaydev787748@gmail.com>
2026-09-19 15:43:54 +0530

    feat: export Phase 0 contracts

---
+++ core/contracts/__init__.py
@@ -0,0 +1,12 @@
+from .capability import Capability, CapabilitySpec
+from .resources import ResourceContract, TaskConstraints
+from .task import Task, TaskStep
+
+__all__ = [
+    "Capability",
+    "CapabilitySpec",
+    "ResourceContract",
+    "TaskConstraints",
+    "Task",
+    "TaskStep",
+]
